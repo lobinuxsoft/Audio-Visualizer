0b4b340539bfb2b11baa0d915ee78c3de8468176
Matias Ezequiel Galarza Fernandez <lobinuxsoft@gmail.com>
2019-04-29 14:44:09 -0300

    Creacion de la frequency band, band buffer e implementacion

--- Assets/Audio Visualizer/Scripts/AudioVisualizer.cs
+++ Assets/Audio Visualizer/Scripts/AudioVisualizer.cs
@@ -6,6 +6,9 @@ public class AudioVisualizer : MonoBehaviour
 {
     public AudioSource audioSource;
     public static float[] samples = new float[512];
+    public static float[] freqBand = new float[8];
+    public static float[] bandBuffer = new float[8];
+    float[] bufferDecrease = new float[8];
 
     // Start is called before the first frame update
     void Start()
@@ -17,10 +20,68 @@ public class AudioVisualizer : MonoBehaviour
     void Update()
     {
         GetSpectrumAudioSource();
+        MakeFrequencyBands();
+        BandBuffer();
     }
 
     void GetSpectrumAudioSource()
     {
         audioSource.GetSpectrumData(samples, 0, FFTWindow.Blackman);
+    }
+
+    void BandBuffer()
+    {
+        for (int i = 0; i < 8; i++)
+        {
+            if (freqBand[i] > bandBuffer[i])
+            {
+                bandBuffer[i] = freqBand[i];
+                bufferDecrease[i] = 0.005f;
+            }
+
+            if (freqBand[i] < bandBuffer[i])
+            {
+                bandBuffer[i] -= bufferDecrease[i];
+                bufferDecrease[i] *= 1.2f;
+            }
+        }
+    }
+
+    void MakeFrequencyBands()
+    {
+        /*
+         Frequency Bands Range
+         0 - 2 = 86 hertz
+         1 - 4 = 172 hertz - 87-258
+         2 - 8 = 344 hertz - 259-602
+         3 - 16 = 688 hertz - 603-1290
+         4 - 32 = 1376 hertz - 1291-2666
+         5 - 64 = 2752 hertz - 2667-5418
+         6 - 128 = 5504 hertz - 5419-10922
+         7 - 256 = 11008 hertz - 10923-21930
+         */
+
+        int count = 0;
+
+        for (int i = 0; i < 8; i++)
+        {
+            float average = 0;
+            int sampleCount = (int)Mathf.Pow(2, i) * 2;
+
+            if (i == 7)
+            {
+                sampleCount += 2;
+            }
+
+            for (int j = 0; j < sampleCount; j++)
+            {
+                average += samples[count] * (count + 1);
+                count++;
+            }
+
+            average /= count;
+
+            freqBand[i] = average * 10;
+        }
     }
 }
---
+++ Assets/Audio Visualizer/Scripts/ParamCube.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ParamCube : MonoBehaviour
+{
+    public int band;
+    public float startScale, scaleMultiplier;
+    public bool useBuffer = true;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (useBuffer)
+        {
+            transform.localScale = new Vector3(
+                                            transform.localScale.x,
+                                            (AudioVisualizer.bandBuffer[band] * scaleMultiplier) + startScale,
+                                            transform.localScale.z
+                                           );
+        }
+        else
+        {
+            transform.localScale = new Vector3(
+                                            transform.localScale.x,
+                                            (AudioVisualizer.freqBand[band] * scaleMultiplier) + startScale,
+                                            transform.localScale.z
+                                           );
+        }
+           
+    }
+}
